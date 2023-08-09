00d36d0521e55a0353d11d2ca19fde72e1ebdf4e
sumitkumar <sumitkumar1202@gmail.com>
2023-08-10 02:57:21 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,94 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:note_x/firebase_options.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized;
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Note X',
+      themeMode: ThemeMode.system,
+      theme: ThemeData(
+        colorScheme: const ColorScheme.light(),
+        appBarTheme: const AppBarTheme(color: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      darkTheme: ThemeData(
+        colorScheme: const ColorScheme.dark(),
+        appBarTheme: const AppBarTheme(color: Colors.black),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override
+  void initState() {
+    super.initState();
+    _email = TextEditingController();
+    _password = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Note X',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              if (user?.emailVerified ?? false) {
+                print('Verified user');
+              } else {
+                print('Not verified user');
+              }
+              return Container();
+            default:
+              return const Center(
+                child: CircularProgressIndicator.adaptive(),
+              );
+          }
+        },
+      ),
+    );
+  }
+}
